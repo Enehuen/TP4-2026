adc33f16698ed785462aaf30006ebd4d982acc81
Enehuen <animatorezequiel@gmail.com>
2026-09-03 11:15:29 -0300

    Realizacion y documentacion del ejercicio2

--- ejercicios/ejercicio2/ejercicio2.c
+++ ejercicios/ejercicio2/ejercicio2.c
@@ -2,7 +2,59 @@
  * Entrega 4 - Ejercicio 2
  * Programación 1 - Ingeniería en Computación - UNRN Andina
  * -----------------
- * Nombre y Apellido
- * Usuario Github
+ * Ezequiel Nehuen
+ * Enehuen
  */
+#include <stdio.h>
 
+int fibonacci(int posicion)
+{
+    int inicio = 0;
+    int secuencia = 1;
+    int resultado;
+    int contador = 1;
+    int temp;
+    if (posicion == 0)
+    {
+        resultado = inicio;
+    }
+    else if (posicion == 1)
+    {
+        resultado = secuencia;
+    }
+    else if (posicion > 1)
+    {
+        while (contador < posicion)
+        {
+            temp = inicio + secuencia;
+            inicio = secuencia;
+            secuencia = temp;
+            contador++;
+        }
+        resultado = temp;
+    }
+    else
+    {
+        resultado = -1;
+    }
+    return resultado;
+}
+
+int suma_fibonacci(int hasta_posicion)
+{
+    int temp;
+    int resultado = 0;
+    if (hasta_posicion < 0)
+    {
+        resultado = -1;
+    }
+    else
+    {
+       for (int i = 0; i <= hasta_posicion; i++)
+        {
+            temp = fibonacci(i);
+            resultado += temp;
+        }    
+    }
+    return resultado;
+}
--- ejercicios/ejercicio2/ejercicio2.h
+++ ejercicios/ejercicio2/ejercicio2.h
@@ -2,7 +2,30 @@
  * Entrega 4 - Ejercicio 2
  * Programación 1 - Ingeniería en Computación - UNRN Andina
  * -----------------
- * Nombre y Apellido
- * Usuario Github
+ * Ezequiel Nehuen
+ * Enehuen
  */
 
+#ifndef EJERCICIO2_H
+#define EJERCICIO2_H
+
+/**
+ * @brief Funcion que mediante la iteracion realiza una suma sucesiva del primer termino con el segundo.
+ * 
+ * @param posicion hasta donde llega la iteracion
+ * @note Intentar ejecutar numeros negativos dara como resultado codigo de error -1.
+ * @return el numero correspondiente a la posicion en la sucesion de fibonacci.
+ */
+
+int fibonacci(int posicion);
+
+/**
+ * @brief Funcion que mediante la iteracion realiza una suma sucesiva de los numeros de fibonacci hasta la posicion indicada.
+ * 
+ * @param hasta_posicion que se realiza la suma
+ * @note Intentar ejecutar numeros negativos dara como resultado codigo de error -1.
+ * @return suma de todos los factores hasta la posicion indicada.
+ */
+int suma_fibonacci(int hasta_posicion);
+
+#endif
