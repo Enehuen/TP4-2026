9d05f8875d20f78866ddffc1fb8c68a31c0025c1
Enehuen <animatorezequiel@gmail.com>
2026-09-03 16:00:52 -0300

    Resolucion del ejercicio3 con documentacion

--- ejercicios/ejercicio3/ejercicio3.c
+++ ejercicios/ejercicio3/ejercicio3.c
@@ -6,3 +6,48 @@
  * Usuario Github
  */
 
+#include <stdio.h>
+
+long long factorial(int numero)
+{   
+    long long resultado = 1;
+    if (numero == 0 || numero == 1)
+    {
+        resultado = 1;
+    }
+    else if (numero < 0)
+    {
+        resultado = -1;
+    }
+    else
+    {
+        for (long long i = 2; i <= numero; i++)
+        {
+            resultado *= i;
+        }
+    }
+    
+    return resultado;
+}
+
+long long combinatorio(int cantidad_elementos, int elementos_por_grupo)
+{
+    long long combinaciones;
+    long long factorial_elementos = factorial(cantidad_elementos);
+    long long factorial_grupos = factorial(elementos_por_grupo);
+    if (elementos_por_grupo < 0 || elementos_por_grupo > cantidad_elementos || cantidad_elementos < 0)
+    {
+        combinaciones = -1;
+    }
+    else if (elementos_por_grupo == 0)
+    {
+        combinaciones = 1;
+    }
+    else
+    {
+        int resta_division = cantidad_elementos - elementos_por_grupo;
+        long long division = factorial(resta_division) * factorial_grupos;
+        combinaciones = factorial_elementos / division;
+    }
+    return combinaciones;
+}
--- ejercicios/ejercicio3/ejercicio3.h
+++ ejercicios/ejercicio3/ejercicio3.h
@@ -2,7 +2,29 @@
  * Entrega 4 - Ejercicio 3
  * Programación 1 - Ingeniería en Computación - UNRN Andina
  * -----------------
- * Nombre y Apellido
- * Usuario Github
+ * Ezequiel Nehuen
+ * Enehuen
  */
 
+/**
+ * @brief Funcion que mediante la iteracion realiza una suma sucesiva
+ * entre los todos los numeros enteros desde 1 hasta el numero ingresado.
+ * 
+ * @param numero al cual se factoriza
+ * @note Intentar ejecutar numeros negativos dara como resultado codigo de error -1.
+ * @return resultado de la multiplicacion desde 1 hasta el numero.
+ */
+long long factorial(int numero);
+
+
+/**
+ * @brief Funcion que utilizando la funcion factorial, realiza el calculo correspondiente
+ * para determinar todos las combinaciones posibles entre los parametros dados.
+ * 
+ * @param cantidad_elementos disponibles del conjunto inicial
+ * @param elementos_por_grupo tomados del conjunto inicial
+ * @note Intentar ejecutar numeros negativos en ambos param dara como resultado codigo de error -1.
+ *          Si elementos_por_grupo es mayor a cantidad de elementos dara como resultado codigo de error -1.
+ * @return cantidad de combinaciones posibles entre los parametros.
+ */
+long long combinatorio(int cantidad_elementos, int elementos_por_grupo);
